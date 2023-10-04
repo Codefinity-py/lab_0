8591f8be4e8483c1119fd8908b62f7e19f59ba52
“podliyPadonak” <supercell0boombeach@gmail.com>
2023-10-04 22:24:43 +0300

    'Solution'

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-name =
-age =
-city =
-country =
+name = 'Ivan'
+age = '19'
+city = 'Lybny'
+country = 'Ukraine'
 
 print(f'My name is {name}')
+print(f'I am {age} years old')
+print(f'I am from {city}, {country}')
