45a7b389d70c4997563fea093ca4c2aa0fdd2717
DanyilZaitsev <danyil.zaitsev@gmail.com>
2023-10-05 12:52:13 +0300

    Solution

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-name =
-age =
-city =
-country =
+name = "Danyil Zaitsev"
+age = 20
+city = "Ivano-Frankivsk"
+country = "Ukraine"
 
 print(f'My name is {name}')
+print(f'I am {age} years old')
+print(f'I am from {country}, {city}')
