22a065769b4dbbf8bc3746efcc18239b6e806fbc
Denys12109 <denys.smetaniak@hebron-academy.com>
2023-10-05 15:03:49 +0300

    solution

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-name =
-age =
-city =
-country =
-
+name = "Denys"
+age = "22"
+city = "Lviv"
+country = "Ukraine"
 print(f'My name is {name}')
+print(f'I am {age} years old')
+print(f'I am from {city}, {country}')
