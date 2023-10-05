3424cc27e507ac3bf742431494dcaf923d3cf768
SergiyShcherbakov <grod13245@gmail.com>
2023-10-05 14:48:12 +0300

    Solution

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-name = "Anastasiia"
-age =
-city =
-country =
+name = 'Sergey'
+age = '20'
+city = 'Zhytomyr'
+country = 'Ukraine'
 
 print(f'My name is {name}')
+print(f'I am {age} years old')
+print(f'I am from {city},{country}')
