b156216fd39fd3d0231928d8b8d473883c14b2f4
hs-user <hs-user@hs-users-MacBook-Pro.local>
2023-10-05 16:50:10 +0300

    Solution

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-name =
-age =
-city =
-country =
+name = 'Pavlo'
+age ='20'
+city = 'Zhytomyr'
+country = 'Ukraine'
 
 print(f'My name is {name}')
+print(f'I am {age} years old')
+print(f'I am from {city}, {country}')
+
+
+
