4dccf2bb7239a8f754483136fabb29ea269a752c
VadymRybachek <ryb.vadymchek@gmail.com>
2023-10-06 11:36:34 +0300

    Solution

--- main.py
+++
@@ -1,7 +0,0 @@
-name = print(f'Me name is {vadym}')
-age = print(f'I am {20} years old')
-city  country = print(f'I am from {lviv,ukreine}')
-
-print(f'Me name is {Vadym}')
-print(f'I am {20} years old')
-print(f'I am from {bgugLviv,Ukreine}')
