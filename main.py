bd77aac1fa3356e3ce9e12cfe19df03f2866268e
Mykhailo <meshagromov2012@gmail.com>
2023-10-06 23:23:14 +0300

    exercise

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-name = Mykhailo
+name = 'Mykhailo'
 age = 20
-city = Kharkiv
-country = Ukreine
+city = 'Kharkiv'
+country = 'Ukreine'
 
-print(f'My name is {Mykhailo}')
+print(f'My name is {name}')
+print(f'I am {age} years old')
+print(f'I am from {city}, {country}')
