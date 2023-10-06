d698b8e1ce04f2e5705df0d11d91cb54ac66955b
VadymRybachek <ryb.vadymchek@gmail.com>
2023-10-06 11:05:26 +0300

    Solution

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-name = print(f'Me name is {Vadym}')
+name = print(f'Me name is {vadym}')
 age = print(f'I am {20} years old')
-city and country = print(f'I am from {Lviv,Ukreine}')
+city  country = print(f'I am from {lviv,ukreine}')
 
 print(f'Me name is {Vadym}')
 print(f'I am {20} years old')
