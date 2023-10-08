28c6e6e9d7b45ead4dd8105361695f272c249490
KolesnykVadym <kolesnikv783@gmail.com>
2023-10-09 00:03:13 +0300

    KolesnykLab_1py

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-name =
-age =
-city =
-country =
+name = "Vadym"
+age = 24
+city = "Doroshivka"
+country = "Ukraine"
 
 print(f'My name is {name}')
+print(f'My name is {age}')
+print(f'My name is {city}')
+print(f'My name is {country}')
