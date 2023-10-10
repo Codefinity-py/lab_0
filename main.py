b71ee74c202fb532db7064da427a2a6b3b211f2d
Tanyusha <tatyanik05@gmail.com>
2023-10-10 12:51:08 +0300

    Solution

--- main.py
+++ main.py
@@ -3,6 +3,6 @@ age = '18'
 city = 'Olexandriya'
 country = 'Ukraine'
 
-print(f'My name is Tetyana')
-print(f'I am 18 years old')
-print(f'I am from Olexandriya, Ukraine')
+print(f'My name is {name}')
+print(f'I am 18 years {age}')
+print(f'I am from {city}, {country}')
