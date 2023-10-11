460a6ab762860de03048afd48fee5cb02a47f666
Vitalii Brydinskyi <vasbrydinskiy@gmail.com>
2023-10-11 21:45:25 +0300

    Solution

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-name =
-age =
-city =
-country =
+name = "Vasyl Brydinskiy"
+age =  19
+city = "lviv"
+country = "Ukraine"
 
 print(f'My name is {name}')
+print(f'My age is {age}')
+print(f'My city is {city}')
+print(f'My country is {country}')
