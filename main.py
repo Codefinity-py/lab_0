1c240d88a7dcee00637e9af3fda685bab1bbe0f6
“HannaMotorova” <phantasmagooorical@gmail.com>
2023-11-01 18:45:01 +0200

    Solution

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-name =
-age =
-city =
-country =
+name = 'Hanna'
+age = 19
+city = 'Druzhkivka'
+country = 'Ukraine'
 
 print(f'My name is {name}')
+print(f'I am {age} years old')
+print(f'I am from {city}, {country}')
